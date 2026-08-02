2a18956a11b778fed95cc46db0d792d05ba2f53f
S M Tamjid Hossain Epick <epickhossain970@gmail.com>
2026-08-02 16:13:30 +0600

    fixed error

--- src/modules/payments/payments.service.ts
+++ src/modules/payments/payments.service.ts
@@ -136,12 +136,17 @@ const getMyPaymentHistoryFromDB = async (userId: string, role: Role) => {
   return result;
 };
 
+// payment.service.ts ফাইলের ভেতরে:
 const getPaymentByIdFromDB = async (id: string, user: any) => {
   const payment = await prisma.payment.findUniqueOrThrow({
     where: { id },
     include: {
       rentalRequest: {
-        include: { property: true, client: true },
+        include: { 
+           property: true, 
+           client: true,
+           review: true // 👈 এটি যুক্ত করতে হবে (বা reviews: true)
+        },
       },
     },
   });
